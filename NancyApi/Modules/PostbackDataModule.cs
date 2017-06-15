8f50e6a64531c868e141a4e2e09fb025fb46a576
it-arjan <it-arjan@users.noreply.github.com>
2017-06-15 20:37:23 +0200

    first

---
+++ NancyApi/Modules/PostbackDataModule.cs
@@ -0,0 +1,26 @@
+﻿using MyData;
+using MyData.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace NancyApi.Modules
+{
+    public class PostbackDataModule : Nancy.NancyModule
+    {
+        public PostbackDataModule() : base("api/postbacks")
+        {
+            Get["/"] = _ => Hello(_.SessionId);
+            Get["/{SessionId}"]=  _ => GetPostbacks(_.SessionId);
+        }
+        private List<PostbackData> GetPostbacks(string SessionId)
+        {
+            var db = new DataFactory(MyDbType.EtfDb).Db();
+            return db.GetPostbacksFromToday();
+        }
+        private string Hello(string SessionId)
+        {
+            return "Helloooo :)";
+        }
+    }
+}
---
+++ NancyApi/Modules/RequestLogModule.cs
@@ -0,0 +1,26 @@
+﻿using MyData;
+using MyData.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NancyApi.Modules
+{
+    public class RequestLogModule : Nancy.NancyModule
+    {
+        public RequestLogModule() : base("api/requestlogs")
+        {
+            Get["/"] = _ => Hello(_.SessionId);
+            Get["/{amount}/{SessionId}"]=  _ => GetLogs(_.amount, _.SessionId);
+        }
+
+        private List<RequestLogEntry> GetLogs(int amount, string SessionId)
+        {
+            var db = new DataFactory(MyDbType.EtfDb).Db();
+            return db.GetRecentRequestLog(amount, SessionId);
+        }
+        private string Hello(string SessionId)
+        {
+            return "Helloooo " + SessionId;
+        }
+    }
+}
